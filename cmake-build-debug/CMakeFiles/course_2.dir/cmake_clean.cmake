29496031bad4cd60f97386a001f441b42b9a9fac
Owerkiller <ivanov_es_7856@mail.ru>
2022-02-08 20:42:56 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/course_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/course_2.dir/main.c.obj"
+  "course_2.exe"
+  "course_2.exe.manifest"
+  "course_2.pdb"
+  "libcourse_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/course_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
